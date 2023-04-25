c56174beed0cbeae9fc67bc6af45ff5e27bc6c62
Dyeggo Silva <dyeggo0102@live.com>
2023-04-25 19:42:18 -0300

    class service e repository
    foi criado as classes repository  e service

--- src/main/java/com/dy/workshopmongo/resource/UserResource.java
+++ src/main/java/com/dy/workshopmongo/resource/UserResource.java
@@ -1,25 +1,28 @@
 package com.dy.workshopmongo.resource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dy.workshopmongo.domain.User;
+import com.dy.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria de Fatima", "mf@gmail.com");
-		User dyeggo = new User("2", "Dyeddo Bezerra", "db@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria,dyeggo));
+		
+		List<User> list = service.findAll();
+		
 		return ResponseEntity.ok().body(list);
 	}
 }
